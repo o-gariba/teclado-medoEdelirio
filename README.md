0a9205605e1038826443c10dc51575403c0efeb8
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-03-16 19:17:03 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+# Teclado de atalhos Medo e Delírio em Brasília
+
+### Objetivo do projeto
+
+Criar um site responsivo com alguns botões que, ao serem acionados (touch ou click), tocam algum som famoso no podcast **Medo e Delírio em Brasília** *(que eu sou mto fã)*.  
+
+Usando o projeto base do curso de ***JavaScript para Web: Crie páginas dinâmicas***, da **Alura**, farei adaptações estruturais (responsividade e novas funcionalidades) e estéticas (cores, efeitos e novos sons)
+
+### Passo a passo do desenvolvimento
+
+>1. Foram selecionados os áudios que substituirão os áudios fornecidos pela Alura.
+>2. Foi feita uma cópia do projeto inicial.
+>3. Um novo repositório público foi criado.
+>4. Alterações no CSS que permitam ao site responsividade em qualquer ocasião.
+>5. Alterações no HTML para aumentar a quantidade de botões de áudio disponíveis.
+>6. Em breve se necessário :smile:
+
+### Tecnologias usadas
+
+>>- HTML
+>>- CSS
+>>- JavaScript
+
+### Ajuda e colaborações
+
+São mto bem vindas :)  
+[Entre em contato comigo](wa.me/5516993797689)
---
+++ main.js
@@ -0,0 +1,63 @@
+// é uma convenção o arquivo principal do javascript ter esse nome.
+
+// alert('Olá mundo');
+
+// function tocaPom() {
+//     document.querySelector('#som_tecla_pom').play();
+// }
+
+// Deixando o tocaPom sem os parenteses estamos GUARDANDO a função que reproduz o som no clicar da tecla especificada. Se colocassemos a função com parenteses ela seria executada de imediato, sem uma atribuição.
+
+// document.querySelector('.tecla_pom').onclick = tocaPom;
+
+// Aqui guardamos a NodeList de teclas do teclado do html.
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// Agora precisamos acessar cada uma das teclas individualmente, para passar o onclick, para isso usamos a notação de array para acessar os elmtos da NodeList.
+
+
+    // const listaDeSons = document.querySelectorAll('audio');
+
+    // function tocaSom(index) {
+    // listaDeSons[index].play(); 
+    // }
+
+function tocaSomAlura(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null || elemento.localName !== 'audio') console.log('Elemento inválido');
+    else elemento.play();
+}
+
+// function tocaTecla(onclick) {
+//     listaDeTeclas[onclick].onclick = listaDeSons[onclick];
+// }
+
+// Usando while para fazer a associação das teclas com os sons.
+
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const som = tecla.classList[1];
+    
+    const idAudio = `#som_${som}`;
+
+    tecla.onclick = function () {
+       tocaSomAlura(idAudio); 
+    }
+
+    tecla.onkeydown = function(evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') tecla.classList.add('ativa'); 
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa');
+    }
+}
+
+// FUNÇÕES ANÔNIMAS PODEM SER USADAS PARA PASSAR UMA FUNÇÃO POR REFERENCIA SEM CHAMALA DIRETAMENTE.
+ 
+// Não pude associar o contador ao tocaSom() pois deu o erro de que o site não pode emitir sons indesejaveis.
+
+// Ainda não entendi qual foi a diferença entre o tocaSom() e o tocaSomAlura() no final das contas. Acho que é pq eu não conseguia fazer uma associação entre um numero da tecla apertada e o som correspondente.
